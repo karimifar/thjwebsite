b3a2f08f5b99dc17eccdc1c397699ae71fb2e7c2
Karimifar <mkarimifar@utlocaldminsPro.utsystem.local>
2018-09-12 17:19:58 -0500

    bootstrap added, more story page

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,16 +1,16 @@
 function goResponsive() {
     var mainNav = document.getElementById("main-nav");
-    if (mainNav.className === "navbar main-nav") {
+    if (mainNav.className === "mynavbar main-nav") {
         mainNav.className += " responsive";
     } else {
-        mainNav.className = "navbar main-nav";
+        mainNav.className = "mynavbar main-nav";
     }
 }
-function showDropDown() {
-    var content = document.getElementById("ut-inst-drop-content");
-    if (content.className === "dropdown-content") {
-        content.className += " clicked-dropdown";
-    } else {
-        content.className = "dropdown-content";
-    }
-}
+// function showDropDown() {
+//     var content = document.getElementById("ut-inst-drop-content");
+//     if (content.className === "dropdown-content") {
+//         content.className += " clicked-dropdown";
+//     } else {
+//         content.className = "dropdown-content";
+//     }
+// }
